ad5f59a003a004db51e90b9678c584897b9b4a74
Joel Acquah <joelaj06@gmail.com>
2022-06-18 12:36:28 +0100

    Landing page/ subscription page/ about page implemented

---
+++ delta/Subs/subscribe.js
@@ -0,0 +1,28 @@
+let loc = document.URL;
+if(loc.search("subscribe") != -1){
+    //alert(document.URL)
+    document.getElementById("underLine").style.textDecoration = "underline"
+    document.getElementById("underLine").style.textDecorationColor = "red"
+}
+
+let subBodtrain = document.getElementById("train");
+let subBodeat = document.getElementById("eat");
+
+let trainBod = document.getElementById("parentTrain");
+let eatBod = document.getElementById("parentDiet");
+
+function switchTopicTrain(){
+   // subBodtrain.style.textDecoration = "underline";
+    subBodtrain.style.color = "white";
+    subBodeat.style.color = "grey";
+    eatBod.style.display = "none";
+    trainBod.style.display = "block";
+}
+
+function switchTopicEat(){
+  // subBodeat.style.textDecoration = "underline";
+    subBodeat.style.color = "white";
+    subBodtrain.style.color = "grey";
+    eatBod.style.display = "block";
+    trainBod.style.display = "none";
+}
---
+++ delta/landing-page/index.js
@@ -0,0 +1,78 @@
+
+function slideInAnimation(start, end) {
+    var fadeInContainer = document.querySelector("."+start);
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle(end, entry.isIntersecting);
+
+            if (entry.isIntersecting) {
+                console.log('intersecting...');
+                 observer.unobserve(entry.target);
+            };
+
+        })
+    });
+
+    observer.observe(fadeInContainer);
+    // fadeInContainers.forEach(element => {
+    //     observer.observe(element);
+    // });
+
+
+}
+
+
+slideInAnimation("animation-box", "show-box");
+slideInAnimation("animation-box-personal", "show-box-personal");
+
+
+// scroll to top
+
+var scrollBtn = document.getElementById('scroll-btn');
+var rootElement = document.documentElement;
+window.onscroll = function () { showScrollBtn() }
+
+function showScrollBtn() {
+    var scrollHeight = 300;
+    console.log(scroll);
+    if (document.body.scrollTop > scrollHeight ||rootElement.scrollTop > scrollHeight) {
+        scrollBtn.style.display = 'block';
+        console.log(rootElement.scrollTop);
+    } else {
+        scrollBtn.style.display = 'none';
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+   rootElement.scrollTop = 0;
+}
+
+function openSignInModal() {
+    document.getElementById("signin-modal").style.display = "block";
+    document.getElementById("signin-bg").style.display = "block";
+}
+
+function closeSignInModal() {
+    document.getElementById("signin-modal").style.display = "none";
+    document.getElementById("signin-bg").style.display = "none";
+}
+
+const signinBtn = document.querySelector(".signinBtn");
+const signupBtn = document.querySelector(".signupBtn");
+
+const formBox = document.querySelector(".formBox");
+const closeBtn = document.querySelector("#closeBtn");
+
+signupBtn.onclick = function () {
+    formBox.classList.add("active");
+
+    closeBtn.classList.add("btnColor");
+    // todo implement color change based on current 
+    // state of the form;
+}
+
+signinBtn.onclick = function () {
+    formBox.classList.remove("active");
+    closeBtn.classList.remove("btnColor");
+}
